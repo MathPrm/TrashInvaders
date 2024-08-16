fd80cc7187efb712a851284c71f3e8b658f2c30a
Charly <charlyplanquettepro@gmail.com>
2024-08-16 22:38:07 +0200

    push charly 9l dans js

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+"use strict"
+
+//création du canvas
+const canvas = document.getElementById("canvas")
+//taille du canvas
+canvas.width = 1000
+canvas.height = 1000
+//c'est du dessin en 2D
+const ctx = canvas.getContext("2d")
